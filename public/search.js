87277a745f33cdbe51247f09f385fd7a64668b70
Jj.L <68043771+jjl25@users.noreply.github.com>
2024-05-18 05:52:13 +0000

    v1.2

--- public/search.js
+++ public/search.js
@@ -1,6 +1,105 @@
 // Initialize myLocation variable to store selected coordinates
 var myLocation = [];
 
+// Array of hospital data
+var hospitals = [
+        
+        {name: "Alexandra Hospital Urgent Care Centre (UCC)", location: [1.2870473851672064, 103.80169283762326], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_ward: "1.1 to 2.2 hrs", link: "https://www.ah.com.sg/Pages/OurServices/Urgent-Care-Centre.aspx"},
+        {name: "Changi General Hospital", location: [1.3402507222916804, 103.94957278340561], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_ward: "3.9 to 29 hrs", link: "https://www.cgh.com.sg/patient-care/your-clinic-visit/visiting-the-emergency-department"},
+        {name: "Khoo Teck Puat Hospital", location: [1.4243504773028133, 103.83858952387206], calculated_distance: null, wait_time_doc: "Unable to get data", no_of_patients: "Unable to get data", wait_time_ward: "3.9 to 23 hrs", link: "https://www.ktph.com.sg/i-want-to/visit-A-and-E"},
+        {name: "National University Hospital", location: [1.2951804107887588, 103.78293825874638], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_ward: "3.9 to 7.1 hrs", link: "https://www.nuh.com.sg/our-services/Specialties/Emergency-Medicine/Pages/Visit-Emergency-Department.aspx"},
+        {name: "Ng Teng Fong General Hospital", location: [1.3335141468299205, 103.74597459082267], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_ward: "1.7 to 5.8 hrs", link: "https://www.ntfgh.com.sg/for-patients-and-visitors/Pages/Visit-Emergency-Department.aspx"},
+        {name: "Sengkang General Hospital", location: [1.3958986576429515, 103.89394476726933], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_ward: "4.1 to 18.7 hrs", link: "https://www.skh.com.sg/patient-care/specialties-services/emergency-medicine"},
+        {name: "Singapore General Hospital", location: [1.2783872348331307, 103.83413697237597], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_ward: "4.2 to 12.6 hrs", link: "https://www.sgh.com.sg/patient-care/visiting-specialist/emergency-care-singapore-general-hospital?fireglass_rsn=true"},
+        {name: "Tan Tock Seng Hospital", location: [1.321316768983475, 103.8463984667227], calculated_distance: null, wait_time_doc: "Unable to get data", no_of_patients: "Unable to get data", wait_time_ward: "4.6 to 9.7 hrs", link: "https://www.ttsh.com.sg/Patients-and-Visitors/Medical-Services/Emergency/Pages/Emergency%20Medicine.aspx"}
+
+];
+
+// Call the function to get data when needed
+getDataFromServer();
+
+// Function to render cards
+function renderCards() {
+    var container = document.getElementById('hospitals-container');
+    container.innerHTML = ''; // Clear the container
+    hospitals.forEach(function(hospital) {
+        var card = document.createElement('div');
+        card.className = 'card';
+        var distanceDisplay = hospital.calculated_distance ? hospital.calculated_distance + ' km' : 'Please enter location';
+        var waitTimeDocDisplay = hospital.wait_time_doc ? hospital.wait_time_doc : 'Wait time data not available';
+        var noOfPatientsDisplay = hospital.no_of_patients ? hospital.no_of_patients : 'Number of patients data not available';
+        var waitTimeWardDisplay = hospital.wait_time_ward ? hospital.wait_time_ward : 'Mean wait time data not available';
+        var linkDisplay = hospital.link ? '<a href="' + hospital.link + '" target="_blank">Go to website</a>' : 'Link not available';
+        card.innerHTML = '<h3>' + hospital.name + '</h3>' +
+                         '<p><strong>Distance:</strong> ' + distanceDisplay + '</p>' +
+                         '<p><strong>Waiting time to see doctor:</strong> ' + waitTimeDocDisplay + '</p>' +
+                         '<p><strong>Total number of patients:</strong> ' + noOfPatientsDisplay + '</p>' +
+                         '<p><strong>Median waiting time for admission to ward:</strong> ' + waitTimeWardDisplay + '</p>' +
+                         '<p>' + linkDisplay + '</p>'; // Add link display
+        container.appendChild(card);
+    });
+}
+
+// Function to extract the minimum wait time from the wait_time_ward string
+function getMinWaitTime(waitTimeStr) {
+    var times = waitTimeStr.split(' to ');
+    return parseFloat(times[0]);
+}
+
+// Function to sort the array by minimum wait time for admission
+function waitTimeSort() {
+    hospitals.sort((a, b) => getMinWaitTime(a.wait_time_ward) - getMinWaitTime(b.wait_time_ward));
+}
+
+// -> check codes, why only show up when serach button is pressed?
+
+// Function to get data from the server
+function getDataFromServer() {
+  fetch('/data') // Makes a network request to the server for the resource at '/data'
+    .then(response => { // Receives the response
+      if (!response.ok) { // Checks if the response status is not successful
+        throw new Error('Network response was not ok'); // Throws an error if the response is not ok
+      }
+      return response.json(); // Parses the response body as JSON
+    })
+    .then(data => {
+      // Assign data[0] to wait_time_doc and no_of_patients variables
+      var waitTimeDoc_KTPH = data[0].x1;
+      var noOfPatients_KTPH = data[0].x2;
+      var waitTimeDoc_TTSH = data[0].y1;
+      var noOfPatients_TTSH = data[0].y2;
+      // console.log('x1 = ' + waitTimeDoc_KTPH + ', x2 = ' + noOfPatients_KTPH + ', y1 = ' + waitTimeDoc_TTSH + ', y2 = ' + waitTimeDoc_TTSH);
+
+      // Find KTPH and TTSH in the hospitals array
+      var KTPH = hospitals.find(hospital => hospital.name === "Khoo Teck Puat Hospital");
+      var TTSH = hospitals.find(hospital => hospital.name === "Tan Tock Seng Hospital");
+
+      // Update the wait_time_doc and no_of_patients for Khoo Teck Puat Hospital
+      if (KTPH) {
+        KTPH.wait_time_doc = waitTimeDoc_KTPH + ' mins';
+        KTPH.no_of_patients = noOfPatients_KTPH;
+      }
+      // Update the wait_time_doc and no_of_patients for Tan Tock Seng Hospital
+      if (TTSH) {
+        TTSH.wait_time_doc = waitTimeDoc_TTSH + ' mins';
+        TTSH.no_of_patients = noOfPatients_TTSH;
+      }
+    })
+
+    .catch(error => {
+      console.error('There has been a problem with your fetch operation:', error); // Catches and logs any errors that occur during the fetch operation
+    });
+
+    // Call waitTimeSort function to sort the hospitals by wait time for admission when the page loads
+    waitTimeSort();
+
+    // Then call renderCards function to show the sorted cards
+    renderCards();
+
+    console.log(hospitals);
+
+}
+
 // Function to perform an address search using OneMap's API
 function addressSearch(searchVal, returnGeom, getAddrDetails, pageNum) {
   // Initialize a new XMLHttpRequest object
@@ -55,7 +154,7 @@ function updateDropdown(results) {
       myLocation = [item.LATITUDE, item.LONGITUDE];
       // Print response on console
       console.log(item);
-      console.log('Latitude: ' + item.LATITUDE +', ' + 'Longitude: ' + item.LONGITUDE);
+      console.log('Latitude = ' + item.LATITUDE +', ' + 'Longitude = ' + item.LONGITUDE);
 
       // Display the selected coordinates on the webpage
       // document.getElementById('latitude').textContent = 'Latitude: ' + item.LATITUDE;
@@ -71,35 +170,8 @@ function updateDropdown(results) {
   });
 }
 
-// Function to handle search input changes and initiate a search
-function performSearch() {
-  // Retrieve the value from the search input field
-  var searchInput = document.getElementById('search-input').value;
-  // Perform the address search with the input value
-  addressSearch(searchInput, 'Y', 'Y', 1);
-  // Display the search results dropdown
-  document.getElementById('search-results').style.display = 'block';
-}
-
 // Add an event listener to handle changes to the search input
 // document.getElementById('search-input').addEventListener('input', performSearch);
-
-// Function to handle search button click and calculate distances
-function onSubmit(event) {
-    event.preventDefault();
-    if (myLocation.length === 0) {
-        alert('Please select a location first.'); // Alert if no location is selected
-        return;
-    }
-    var lat = parseFloat(myLocation[0]);
-    var lon = parseFloat(myLocation[1]);
-    hospitals.forEach(function(hospital) {
-        var distance = calculateDistance(lat, lon, hospital.location[0], hospital.location[1]);
-        hospital.calculated_distance = distance; // Store distance in the hospital object
-    });
-    updateDistancesAndSort(); // Sort hospitals by distance
-    renderCards(); // Render the hospital cards
-}
 
 // Function to calculate distance between two coordinates
 function calculateDistance(lat1, lon1, lat2, lon2) {
@@ -120,82 +192,39 @@ function deg2rad(deg) {
     return deg * (Math.PI/180);
 }
 
-// Array of hospital data
-var hospitals = [
-        
-        {name: "Alexandra Hospital Urgent Care Centre (UCC)", location: [1.2870473851672064, 103.80169283762326], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_atw: "1.1 to 2.2 hrs"},
-        {name: "Changi General Hospital", location: [1.3402507222916804, 103.94957278340561], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_atw: "3.9 to 29 hrs"},
-        {name: "Khoo Teck Puat Hospital", location: [1.4243504773028133, 103.83858952387206], calculated_distance: null, wait_time_doc: "Error", no_of_patients: "Error", wait_time_atw: "3.9 to 23 hrs"},
-        {name: "National University Hospital", location: [1.2951804107887588, 103.78293825874638], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_atw: "3.9 to 7.1 hrs"},
-        {name: "Ng Teng Fong General Hospital", location: [1.3335141468299205, 103.74597459082267], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_atw: "1.7 to 5.8 hrs"},
-        {name: "Sengkang General Hospital", location: [1.3958986576429515, 103.89394476726933], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_atw: "4.1 to 18.7 hrs"},
-        {name: "Singapore General Hospital", location: [1.2783872348331307, 103.83413697237597], calculated_distance: null, wait_time_doc: "-", no_of_patients: "-", wait_time_atw: "4.2 to 12.6 hrs"},
-        {name: "Tan Tock Seng Hospital", location: [1.321316768983475, 103.8463984667227], calculated_distance: null, wait_time_doc: "Error", no_of_patients: "Error", wait_time_atw: "4.6 to 9.7 hrs"}
-
-];
-
 // Function to update distances and sort the array
 function updateDistancesAndSort() {
     hospitals.sort((a, b) => parseFloat(a.calculated_distance) - parseFloat(b.calculated_distance));
 }
 
-// Function to render cards
-function renderCards() {
-    var container = document.getElementById('hospitals-container');
-    container.innerHTML = ''; // Clear the container
+// Function to handle search input changes and initiate a search
+function performSearch() {
+  // Retrieve the value from the search input field
+  var searchInput = document.getElementById('search-input').value;
+  // Perform the address search with the input value
+  addressSearch(searchInput, 'Y', 'Y', 1);
+  // Display the search results dropdown
+  document.getElementById('search-results').style.display = 'block';
+}
+
+// Function to handle search button click and calculate distances
+function onSubmit(event) {
+    event.preventDefault();
+    if (myLocation.length === 0) {
+        alert('Please enter location.'); // Alert if no location is selected
+        return;
+    }
+    var lat = parseFloat(myLocation[0]);
+    var lon = parseFloat(myLocation[1]);
     hospitals.forEach(function(hospital) {
-        var card = document.createElement('div');
-        card.className = 'card';
-        var distanceDisplay = hospital.calculated_distance ? hospital.calculated_distance + ' km' : 'Please enter location';
-        var waitTimeDocDisplay = hospital.wait_time_doc ? hospital.wait_time_doc : 'Wait time data not available';
-        var noOfPatientsDisplay = hospital.no_of_patients ? hospital.no_of_patients : 'Number of patients data not available';
-        var waitTimeAtwDisplay = hospital.wait_time_atw ? hospital.wait_time_atw : 'Average wait time data not available';
-        card.innerHTML = '<h3>' + hospital.name + '</h3>' +
-                         '<p><strong>Distance:</strong> ' + distanceDisplay + '</p>' +
-                         '<p><strong>Waiting time to see doctor:</strong> ' + waitTimeDocDisplay + '</p>' +
-                         '<p><strong>Total number of patients:</strong> ' + noOfPatientsDisplay + '</p>' +
-                         '<p><strong>Median waiting time for admission to ward:</strong> ' + waitTimeAtwDisplay + '</p>';
-        container.appendChild(card);
+        var distance = calculateDistance(lat, lon, hospital.location[0], hospital.location[1]);
+        hospital.calculated_distance = distance; // Store distance in the hospital object
     });
+    updateDistancesAndSort(); // Sort hospitals by distance
+    renderCards(); // Render the hospital cards
 }
 
-// Function to extract the minimum wait time from the wait_time_atw string
-function getMinWaitTime(waitTimeStr) {
-    var times = waitTimeStr.split(' to ');
-    return parseFloat(times[0]);
-}
 
-// Function to sort the array by minimum wait time for admission
-function waitTimeSort() {
-    hospitals.sort((a, b) => getMinWaitTime(a.wait_time_atw) - getMinWaitTime(b.wait_time_atw));
-}
 
-// --- New codes, to test
 
-// Function to get data from the server
-function getDataFromServer() {
-  fetch('/data') // Makes a network request to the server for the resource at '/data'
-    .then(response => { // Receives the response
-      if (!response.ok) { // Checks if the response status is not successful
-        throw new Error('Network response was not ok'); // Throws an error if the response is not ok
-      }
-      return response.json(); // Parses the response body as JSON
-    })
-    .then(data => {
-      console.log(data); // Logs the data received from the server
-    })
-    .catch(error => {
-      console.error('There has been a problem with your fetch operation:', error); // Catches and logs any errors that occur during the fetch operation
-    });
-}
 
-// Call the function to get data when needed
-getDataFromServer();
-
-// --- New codes, to test
-
-// Call waitTimeSort function to sort the hospitals by wait time for admission when the page loads
-waitTimeSort();
-
-// Then call renderCards function to show the sorted cards
-renderCards();
